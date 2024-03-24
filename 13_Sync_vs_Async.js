592dc475776446b58b7dbafd0f886287c86e3f2d
OscarSah <oscar@cybertekschool.com>
2024-03-24 12:44:49 -0400

    liveReview second Batch

--- 13_Sync_vs_Async.js
+++ 13_Sync_vs_Async.js
@@ -6,9 +6,9 @@
  * Latest approach in JS is - 'async function' s, 'await' statements
  */
 
-let chars = Array.from('CYDEO'); // like tocharArray() method in JAVA
-console.table(chars);
-
+// let chars = Array.from('CYDEO'); // like tocharArray() method in JAVA
+// console.table(chars);
+/*  THE PROBLEM ASYNC
 let text = '';
 function myScript(value, index) {
     text += `${index}nth element is ${value}\n`; // JS way of string cancatination
@@ -16,16 +16,44 @@ function myScript(value, index) {
 
 function createArray(stringObject) {
     let elements = [];
-    elements = Array.from(stringObject);
+    setTimeout(() => {
+        elements = Array.from(stringObject);
+        console.log('just received the data from dataBase but it is too late');
+    }, 3000);
     return elements;
 }
 let moreArray = createArray('JAVASCRIPT');
 
-chars.forEach(myScript);
-console.log(text);
+text = 'empty';
 
-text = '';
-
-moreArray.forEach(myScript);
+moreArray.forEach(myScript); // this function is not waiting for backend response 3 seconds
 console.log(text);
+*/
+
+// lets use promises to handle this problem
+
+function createArray(stringObject) {
+    return new Promise(function (resolve,reject){
+        if(stringObject){
+            let elements = [];
+            setTimeout(() => {
+                elements = Array.from(stringObject);
+                console.log('just received the data from dataBase NOT LATE ANYMORE');
+                resolve(elements)
+            }, 3000);
+        }else{
+            reject('NO object provided');
+        }
+    })
+}
+let text = '';
+function myScript(value, index) {
+    text += `${index}nth element is ${value}\n`; // JS way of string cancatination
+}
+createArray('JAVASCRIPT').then((arr) => {
+    arr.forEach(myScript);
+    console.log(text);
+}).catch((str) => {
+    console.log(str);
+})
 
